153d8b9912d31eeaefac15f78eb4a9586100642b
misohan <michal.hanusniak@aiesec.cz>
2020-05-19 22:26:22 +0200

    Add overriden methods from Wines

---
+++ src/main/java/alcohol/wines/WhiteWine.java
@@ -0,0 +1,14 @@
+package alcohol.wines;
+
+public class WhiteWine extends Wine {
+
+    @Override
+    public String levelOfExcise() {
+        return super.levelOfExcise();
+    }
+
+    @Override
+    public String hangoverOutcome() {
+        return "Helicopter effect";
+    }
+}
